0be00ecb6ea810dd1ccd8a37c1f772c42de1bf64
markasif <apasif243@gmail.com>
2025-11-04 12:04:35 +0530

    Seo update

--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -10,6 +10,13 @@ import ConnectSection from '../Components/Connect/Connect';
 export default function HomePage() {
   return (
     <>
+    <title>Think Forge Global</title>
+      <meta
+        name="description"
+       content="Think Forge Global is a top AI software and IT company in Perinthalmanna, Malappuram. We lead in digital transformation, data analytics, and web/app development for Kerala."
+      />
+      <link rel="canonical" href="https://www.thinkforgeglobal.com/" />
+      
       <Hero />
       <About />
       <OurServices />
